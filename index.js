4a707de11408307a9bf81cc9131e60fc179e0835
Milana.K.Z <milanakretinina@gmail.com>
2022-12-23 16:05:16 +0000

    hello

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import bodyParser from "body-parser"
 const app = express()
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get ('/', (req, res) =>
 {
-    res.send("Hello, world!");
+    res.render("index.ejs");
+})
+app.post ('/', (req, res) =>
+{
+    const username = req.body.username;
+    res.render('hello.ejs', {username: username})
 })
 const port = process.env.PORT || 8080
 
